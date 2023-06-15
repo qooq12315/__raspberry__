290639e924bb5fc2adcbb5eb7ce1060b477034cb
ooo <qaoo12158@gmail.com>
2023-06-15 15:08:16 +0800

    ok

---
+++ datasource.py
@@ -0,0 +1,27 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+  
+def create_table(conn):
+    sql_projects = """
+    CREATE TABLE IF NOT EXISTS led(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT NOT NULL,
+		state INTEGER NOT NULL		
+    );
+    """
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_projects)
+        print("Table created successfully")  
+    except Error as e:
+        print(e)
