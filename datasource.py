3b5c58dfa8adb56eeea6afb9c0d7b18a0ffc780a
ooo <qaoo12158@gmail.com>
2023-06-15 15:52:18 +0800

    ok

--- datasource.py
+++ datasource.py
@@ -25,3 +25,12 @@ def create_table(conn):
         print("Table created successfully")  
     except Error as e:
         print(e)
+
+def insert_data(conn, state):
+    sql_insert = """
+    INSERT INTO led(date, state) VALUES(datatime('now','localtime'), ?);
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql_insert, (state,))
+    conn.commit()
+    conn.close()
